997503df577934d857622ae065e0b8e0b77806c2
sreelakshmi04 <sreesrelakshmivinod123@gmail.com>
2021-07-03 16:15:55 +0530

    for brave

--- tkmcomplex/routes.py
+++ tkmcomplex/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template ,url_for,flash,redirect
+from flask import render_template ,url_for,flash,redirect,send_from_directory
 from tkmcomplex import app
 from tkmcomplex.data import shops
 
@@ -7,3 +7,6 @@ def home():
     logo=url_for('static',filename='images/TKM_logo_icon.png')
     return render_template('home.html',title='Home',shop=shops,logo=logo)
 
+@app.route("/.well-known/brave-rewards-verification.txt")
+def brave():
+    return send_from_directory("static",".well-known/brave-rewards-verification.txt")
